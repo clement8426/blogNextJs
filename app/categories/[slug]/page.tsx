021c8b516fe2fec3bbfec34c9dcfd51c6efb5732
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2024-02-10 17:35:52 +0100

    first next.js app

---
+++ app/categories/[slug]/page.tsx
@@ -0,0 +1,10 @@
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function CategoriesPage({ params }: Props) {
+  const { slug } = params;
+  return <div>CategoriesPage {slug}</div>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          <p>Header</p>
+        </div>
+        {children}
+        <div>Footer</div>
+      </body>
     </html>
   );
 }
